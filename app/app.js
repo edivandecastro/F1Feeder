56aa19bcfb4be958d566e84d350ed12664ee0b66
Edivan de Castro <edivandecastro@gmail.com>
2016-01-02 13:18:13 -0300

    Added service for drivers replacing array in driverList

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('F1FeederApp', [
   'ngRoute',
-  'F1FeederApp.Driver'
+  'F1FeederApp.Driver',
+  'F1FeederApp.Service'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
--- app/driver/DriverController.js
+++ app/driver/DriverController.js
@@ -1,28 +1,10 @@
 angular.module('F1FeederApp.Driver', [])
 
-.controller('DriverController', function ($scope) {
-  $scope.driverList = [
-    {
-      Driver: {
-        givenName:  'Sebastian',
-        familyName: 'Vettel'
-      },
-      points: 322,
-      nationality: "German",
-      Constructors: [
-        {name: "Red Bull"}
-      ]
-    },
-    {
-      Driver: {
-        givenName:  'Fernando',
-        familyName: 'Alonso'
-      },
-      points: 207,
-      nationality: "Spanish",
-      Constructors: [
-        {name: "Ferrari"}
-      ]
-    }
-  ]
+.controller('DriverController', function ($scope, ErgastApiService) {
+  $scope.nameField = null;
+  $scope.driverList = [];
+
+  ErgastApiService.getDrivers().success(function(response) {
+    $scope.driverList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+  });
 });
